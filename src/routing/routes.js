a343b8bb334fe0ff0781bae7a25d4c481cc4340c
kcunanan <kevin.a.cunanan@gmail.com>
2020-09-23 03:09:35 -0700

    added routes and navigation routes for project

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -1,10 +1,22 @@
 import Home from '_/components/Home/Home';
 
+import ProjectHeader from '_/components/Project/ProjectHeader/ProjectHeader';
+import Project from '../components/Project/Project';
+
 export const defaultPath = '/';
 
 export const routes = [
   {
     name: 'home', path: '/', exact: true, component: Home,
+  },
+  {
+    name: 'project', path: '/projects/:projectSlug', component: Project,
+  },
+];
+
+export const navigationRoutes = [
+  {
+    name: 'project', path: '/projects/:projectSlug', component: ProjectHeader,
   },
 ];
 
